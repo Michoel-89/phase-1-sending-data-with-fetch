5d0bb11812c7c1ed46439b5f718e640609fd15b7
michoel <michoelr111@gmail.com>
2023-05-05 14:37:13 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    })
+    .then(resp => resp.json())
+    .then(obj => {
+        const h = document.createElement('h1');
+        h.textContent = obj.id;
+        document.body.appendChild(h);})
+    .catch(error => {
+        const h = document.createElement('h1');
+        h.textContent = error.message;
+        document.body.appendChild(h);
+    })
+}
+submitData('joe', 'balmer');
